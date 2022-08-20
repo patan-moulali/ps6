815660139a16d7a9631c373aa1211ee9e0fe6ec5
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-20 14:58:35 +0530

    Add project files.

---
+++ ps6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.Write("Enter your age ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("You look younger than  {0}", age);
+            Console.Read();
+        }
+    }
+}
